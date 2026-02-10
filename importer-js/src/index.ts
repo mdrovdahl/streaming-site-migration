9af10f6c079c876f59273863b4c1cfb0f3042528
furiosa <mark.drovdahl@automattic.com>
2026-02-10 15:44:31 -0800

    feat: add public API index.ts with exports (ssmj-hvl)

---
+++ importer-js/src/index.ts
@@ -0,0 +1,4 @@
+export { importSite } from './import-orchestrator';
+export { PlaygroundImportTarget } from './playground-sink';
+export type { PlaygroundClient } from './playground-sink';
+export type { ImportConfig, ImportProgress, ImportTarget } from './types';
